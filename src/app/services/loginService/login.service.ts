af0aa3782787453899d08555bdbbff43ce75b864
adammoveo <adamo@moveo.co.il>
2023-01-25 14:09:57 +0200

    more

--- src/app/services/loginService/login.service.ts
+++ src/app/services/loginService/login.service.ts
@@ -1,20 +1,37 @@
-import { Observable } from "rxjs";
+import { BehaviorSubject, Observable, Subject, tap } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { getCookie, setCookie,removeCookie } from "typescript-cookie";
+import { AdminService } from "../adminService/admin.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class LoginService {
   url = "http://localhost:8000/api/v1";
-  constructor(private httpClient: HttpClient) {}
+ private _isLoggedIn$ = new BehaviorSubject<Boolean>(false);
+ isLoggedIn$ = this._isLoggedIn$.asObservable();
+
+  constructor(private httpClient: HttpClient,
+    private adminService:AdminService) {
+    const token = getCookie('token');
+    this._isLoggedIn$.next(!!token);//TODO check expiration date
+  }//TODO token service that handles all the verification (maybe not exclusive to tokens)
 
   login(username: string, password: string): Observable<Object> {
     return this.httpClient.post(`${this.url}/auth`, {
       username,
       password,
-    });
+    }).pipe(
+      tap((res:any) => {
+        this._isLoggedIn$.next(true);
+        console.log(" this._isLoggedIn$.next(true);");
+        this.adminService.setAdmin(res.user.admin);
+        setCookie("login", res.user.name);
+        setCookie("token", res.token);
+        setCookie("admin", res.user.admin);//TODO role
+      })
+    );
   }
 
   checkedLogin(): boolean {
@@ -22,6 +39,12 @@ export class LoginService {
   }
 
   logout(){
+    this._isLoggedIn$.next(false);
     removeCookie('login');
+    removeCookie('token');
+  }
+
+  getUsername(){
+    return getCookie('login');
   }
 }
--- src/app/shared/views/login/login.component.ts
+++ src/app/shared/views/login/login.component.ts
@@ -1,51 +1,63 @@
-import { Router } from '@angular/router';
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import IUser from 'src/app/models/User';
-import { LoginService } from 'src/app/services/loginService/login.service';
-import { getCookie, setCookie } from 'typescript-cookie'
+import { Router } from "@angular/router";
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import IUser from "src/app/models/User";
+import { LoginService } from "src/app/services/loginService/login.service";
+import { getCookie, setCookie } from "typescript-cookie";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AuthInterceptor } from "src/app/services/auth.interceptor";
+import { AdminService } from "src/app/services/adminService/admin.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
+  providers:[ {
+    provide:HTTP_INTERCEPTORS, //DI
+    useClass:AuthInterceptor,
+    multi:true // there can be multi http interceptors (one for every req)
+  }]
 })
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,
-    private loginService:LoginService,
-    private route:Router) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private loginService: LoginService,
+    private route: Router,
+    private adminService:AdminService
+  ) {}
 
   ngOnInit() {
-    if(this.loginService.checkedLogin()){
-      this.route.navigate(['restaurant'])
+    
+    if (this.loginService.checkedLogin()) {
+      this.route.navigate(["restaurant"]);
     }
     this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
+      username: ["", Validators.required],
+      password: ["", Validators.required],
     });
   }
 
   onSubmit() {
-    const {username,password} = this.loginForm.value;
-   this.loginService.login(username,password).subscribe({
-    next:(user:any) => {
-      console.log(user);
-      
-      setCookie('login',user.username)
-this.route.navigate(['restaurant'])
-    },
-    error:(err) => console.log(err)
-   })
+    const { username, password } = this.loginForm.value;
+    this.loginService.login(username, password).subscribe({
+      next: (data: any) => {
+        
+       
+        this.route.navigate(["restaurant"]);
+        // this.loginService.LoginEmitter.next(true);
+      },
+      error: (err) => console.log(err),
+    });
   }
 
   get password() {
-    return this.getField('password')
+    return this.getField("password");
   }
 
   get username() {
-    return this.getField('username')
+    return this.getField("username");
   }
 
   private getField(field: string) {
